320ee25381491e75b4bcca518878386680c11edc
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2018-10-01 20:06:21 +0530

    Update test.java
    Commit 2.0

--- Stack Implementaion/test.java
+++ Stack Implementaion/test.java
@@ -1,23 +1,45 @@
 package stack;
 
-public class test {
-
-	public static void main(String[] args) {
-	stack<Integer> S =new Stack_Array<Integer>();
-	System.out.println("Size "+S.size());
-	S.push(5);
-	S.push(4);
-	S.push(8);
-	S.push(0);
-	S.push(7);
-	S.push(10);
-	S.push(3);
-	S.push(1);
-	System.out.println(S.pop());
-	System.out.println(S.top());
-	S.printStack();
-	System.out.println("Size "+S.size());
-
-	}
-
-}
+ import java.util.Scanner;
+     
+    public class StackFinal2 {
+     
+    	/**
+    	 * @param args
+    	 */
+    	public static void main(String[] args) {
+    		
+    		Scanner scan = new Scanner(System.in);
+    		CapacityGetterSetter getset = new CapacityGetterSetter();
+    		int testcases = Integer.parseInt(scan.nextLine());
+    		int stacksize = Integer.parseInt(scan.nextLine());
+    		getset.setCapacity(stacksize);
+    		stack<Integer> S = new StackArray<Integer>();
+    		int inputs = Integer.parseInt(scan.nextLine());
+     
+    		while (inputs >0) {
+    			String operation = scan.nextLine();
+    			String[] temp = operation.split(" ");
+    			if (temp[0].equals("S"))
+    				System.out.println(S.size());
+    			if (temp[0].equals("I"))
+    				System.out.println(S.isEmpty());
+    			if (temp[0].equals("P")){
+    				S.push(Integer.parseInt(temp[1]));
+    				
+    				
+    			}
+    			if (temp[0].equals("O")){
+    				System.out.println(S.pop());
+    				S.printStack();
+    			}
+    			if (temp[0].equals("T"))
+    				System.out.println(S.top());
+    			inputs--;
+    			
+    				
+    			}
+    		}
+     
+    	}
+    }
